a08b52513dfb9d5acc68f95e3f009b9d6a8288aa
Sam Bowden <srb55@student.le.ac.uk>
2023-02-25 17:31:27 +0000

    Initial code

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ghc -O2 insulin_calc.hs
---
+++ insulin_calc.hs
@@ -0,0 +1,67 @@
+import qualified Data.Map.Strict as Map
+import System.IO
+import Text.Read
+import Text.Printf
+
+type Carbs = Float
+type Name = String
+type Weight = Float
+type Multiplicity = Float
+type Item = (Name, Weight, Multiplicity)
+type Dose = Float
+type Database = Map.Map Name Carbs
+type CarbRatio = Int
+type CarbRatios = Map.Map Name CarbRatio
+type Meal = [Item]
+data Result a b = Ok a | Err b
+
+sumCarbs :: Meal -> Database -> Result Carbs [Name]
+sumCarbs [] db = Ok 0
+sumCarbs ((name, weight, mult):items) db = case (Map.lookup name db, sumCarbs items db) of
+  (Just per_100g, Ok sum) -> Ok ((weight * (per_100g / 100) * mult) + sum) 
+  (Just _,  Err names) -> Err names
+  (Nothing, Ok _) -> Err [name]
+  (Nothing, Err names) -> Err (name:names)
+
+calcDose :: Carbs -> CarbRatio -> Dose
+calcDose sum carb_ratio = sum / fromIntegral carb_ratio
+
+getItems :: IO [Item]
+getItems = do
+  putStr "> "
+  hFlush stdout
+  e <- getLine
+  case words e of
+    [n, w, m] -> case (readMaybe w, readMaybe m) of
+      (Just w, Just m) -> do
+        xs <- getItems
+        return ((n, w, m):xs)
+      _ -> do
+        putStrLn "Invalid format: weight and multiplicity must be valid numbers!"
+        getItems
+    [":d"] -> return ([])
+    _ -> do
+      putStrLn "Invalid format: there must be three space separated arguments!"
+      getItems
+     
+main :: IO ()
+main = do
+  putStr "Carb ratio name: "
+  hFlush stdout
+  input <- getLine
+  case Map.lookup input carb_ratios of
+    Just carb_ratio -> do
+      putStrLn "Enter items (<name> <weight> <multiplicity>). Type :d to finish."
+      meal <- getItems
+      case sumCarbs meal db of
+        Ok sum -> do
+          printf "Total carbs: %.2fg\n" sum
+          printf "Calculated insulin dose: %.2f units\n" (calcDose sum carb_ratio)
+        Err items -> putStrLn $ "Items not found in database: " ++ show items
+    Nothing -> putStrLn $ "Carb ratio with name '" ++ input ++ "' has not been set"
+
+-- Database and carb ratios temporarily in code while prototyping
+db :: Database
+db = Map.fromList [("Apple", 10), ("Banana", 20.9), ("Orange", 10)]
+carb_ratios :: CarbRatios
+carb_ratios = Map.fromList [("Breakfast", 7), ("Dinner", 11), ("Tea", 10)]
